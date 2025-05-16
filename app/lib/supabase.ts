0bc0ff81e19f752dc02ba31d4210a85b9457297f
Douglas Smith <douglass226@outlook.com>
2025-05-15 20:31:27 -0400

    git ignore

---
+++ app/lib/supabase.ts
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://asvyqpcheguhseuneoqz.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzdnlxcGNoZWd1aHNldW5lb3F6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYyMTI0MDUsImV4cCI6MjA2MTc4ODQwNX0._wz_X7KqVz3gayjAz-R2byVUu6awopEFEVqtardDSWE';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  realtime: { enabled: false },
+});
